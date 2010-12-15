4618df568f7faf4955f8b25698d0bb5f37c5c1e5
Stefano Rivera <stefano@rivera.za.net>
2010-12-15 15:38:51 +0200

    List minor Python versions supported in classifiers

--- setup.py
+++ setup.py
@@ -67,6 +67,11 @@ setup(name='objgraph',
       long_description=get_description(),
       classifiers=[
           'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.1',
+          'Programming Language :: Python :: 3.2',
       ],
       py_modules=['objgraph'])
