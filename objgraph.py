22b78e5df56bce50b45e050feec35efe2a77df9e
Patrick Costello <pcostello@google.com>
2015-02-19 10:52:33 -0500

    Fixed formatting.

--- objgraph.py
+++ objgraph.py
@@ -29,7 +29,7 @@ Released under the MIT licence.
 __author__ = "Marius Gedminas (marius@gedmin.as)"
 __copyright__ = "Copyright (c) 2008-2014 Marius Gedminas"
 __license__ = "MIT"
-__version__ = "1.9.0dev0"
+__version__ = "1.9.0.dev0"
 __date__ = "2014-05-15"
 
 
