03b6145ff82752e4a30c8e9a2df5b759bb44717c
Marius Gedminas <marius@gedmin.as>
2017-12-20 11:02:41 +0200

    Modernize the setup.py
    - Use io.open() instead of codecs.open()
    
    - Drop the unused unichr() definition for Python 3
    
    - Add zip_safe = True (although I hate zipped eggs)
    
    - Add keywords
    
    - Reindent setup() parameters

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-import codecs
 import doctest
+import io
 import os
 import re
 import sys
@@ -8,19 +8,10 @@ import unittest
 
 from setuptools import setup
 
-try:
-    unichr
-except NameError:
-    # Python 3.x support
-    unichr = chr
-
 
 def read(filename):
-    f = codecs.open(filename, 'r', 'utf-8')
-    try:
+    with io.open(filename, encoding='UTF-8') as f:
         return f.read()
-    finally:
-        f.close()
 
 
 def unsphinx(text):
@@ -67,30 +58,34 @@ if len(sys.argv) > 1 and sys.argv[1] == '--build-images':
     sys.exit(0)
 
 
-setup(name='objgraph',
-      version=get_version(),
-      author='Marius Gedminas',
-      author_email='marius@gedmin.as',
-      url='https://mg.pov.lt/objgraph/',
-      license='MIT',
-      description='Draws Python object reference graphs with graphviz',
-      long_description=get_description(),
-      classifiers=[
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-      ],
-      py_modules=['objgraph'],
-      install_requires=[
-          'graphviz',  # just for ipython support currently
-      ],
-      tests_require=['mock'],
-      test_suite='tests.test_suite')
+setup(
+    name='objgraph',
+    version=get_version(),
+    author='Marius Gedminas',
+    author_email='marius@gedmin.as',
+    url='https://mg.pov.lt/objgraph/',
+    license='MIT',
+    description='Draws Python object reference graphs with graphviz',
+    long_description=get_description(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='object graph visualization graphviz garbage collection',
+    py_modules=['objgraph'],
+    install_requires=[
+        'graphviz',  # just for ipython support currently
+    ],
+    tests_require=['mock'],
+    test_suite='tests.test_suite',
+    zip_safe=True,
+)
