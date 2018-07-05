09cc5c0566a3efb603e6afca0b77325c0b9b2631
Yu Feng <rainwoodman@gmail.com>
2018-07-05 11:40:45 -0700

    Speed up at_addrs with a dict.
    The old loop was O(mn). This is O(m+n).

--- objgraph.py
+++ objgraph.py
@@ -582,9 +582,12 @@ def at_addrs(address_set):
     .. versionadded:: 3.4
     """
     res = []
-    for o in gc.get_objects():
-        if id(o) in address_set:
-            res.append(o)
+    id_to_obj = dict((id(o), o) for o in gc.get_objects())
+    
+    for i in address_set:
+        o = id_to_obj[i]
+        res.append(o)
+
     return res
 
 
